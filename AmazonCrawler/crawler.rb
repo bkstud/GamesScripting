b9c02f9d68f4f49fe620157114279b2b8ebbac5d
bkstud <bkstud@em.com>
2023-06-09 17:13:26 +0200

    working on detailed description

--- AmazonCrawler/crawler.rb
+++ AmazonCrawler/crawler.rb
@@ -5,13 +5,7 @@ require 'open-uri'
 require "net/https"
 require "uri"
 
-BASE_PAGE = "https://amazon.com"
-# https://www.amazon.com/s?k=graphics+card
-keyword = "graphics card"
-#url = "https://www.amazon.com/s?i=specialty-aps&bbn=16225009011&rh=n%3A%2116225009011%2Cn%3A541966&ref=nav_em__nav_desktop_sa_intl_computers_and_accessories_0_2_5_6"
-url = 'https://www.amazon.com/s?k=graphics+card'
-
-headers = {
+HEADERS = {
     'dnt' => '1',
     'upgrade-insecure-requests' => '1',
     'user-agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36',
@@ -24,9 +18,19 @@ headers = {
     'accept-language' => 'en-GB,en-US;q=0.9,en;q=0.8',
 }
 
-puts url
+BASE_PAGE = "https://amazon.com"
+
+# search keyword
+keyword = "graphics card"
+# TODO: base on keyword
+search_url = 'https://www.amazon.com/s?k=graphics+card'
+# number of pages to fetch from
+number_of_pages = 1
+
+
+puts search_url
 # open uri
-call = URI.open(url, **headers)
+call = URI.open(search_url, **HEADERS)
 puts call.status
 #File.open("out.html", 'w') { |file| file.write(call.read()) }
 # Parse HTML document
@@ -37,9 +41,25 @@ i = 0
 doc.css('div[data-component-type="s-search-result"]').each do |item|
   link = item.css('h2 a.a-link-normal.a-text-normal')
   title = link.text
-  url = link.attribute('href').value
-  puts i
+  url = BASE_PAGE + link.attribute('href').value
+  price = item.css('span.a-price:nth-of-type(1) span.a-offscreen')
+  price = price.text
+
+  item_call = URI.open(url, **HEADERS)
+  item_doc = Nokogiri::HTML(item_call)
+  description = item_doc.css('#productDescription')
+  if ! description
+    puts "failed to find desc"
+  else
+    description = description.text.lstrip
+  end
+  
+  puts "number: " + String(i)
+
   puts title
+  puts price
   puts url
+  puts description
+
   i += 1
 end
